fe3307e68a8a9cf9e596ae10dc3e2d9644a2068e
Maksym Dryha <maksym_dryha@epam.com>
2018-09-12 00:21:04 +0300

    implement findEqualObject function

--- src/search/findEqualObject/findEqualObject.js
+++ src/search/findEqualObject/findEqualObject.js
@@ -1,4 +1,9 @@
+const isEqual = require('../../helpers/isEqual');
+const isObject = require('../../helpers/isObject')
+
 function findEqualObject(arr, object) {
+  const error = checkErrors(arguments);
+  if(error) throw error;
   for (let i = 0; i < arr.length; i++) {
     if (isEqual(arr[i], object)) {
       return i;
@@ -6,3 +11,18 @@ function findEqualObject(arr, object) {
   }
   return -1;
 };
+
+function checkErrors(args) {
+  if (args.length < 2) {
+    return new Error('Expected 2 arguments');
+  }
+  if (!Array.isArray(args[0])) {
+    return new Error('The first argument should be an array');
+  }
+  if(!isObject(args[1])) {
+    return new Error('The second argument should be an object');
+  }
+  return false;
+}
+
+module.exports = findEqualObject;
--- src/search/findEqualObject/findEqualObject.test.js
+++ src/search/findEqualObject/findEqualObject.test.js
@@ -1,56 +1,49 @@
-const findEqualObject = require('./findEqualObject');
+const findEqualObject = require("./findEqualObject");
 
-test('stub', () => {
-  return;
-})
+test("returns correct index", () => {
+  const objectsArr = [
+    { x: 1, y: 3, z: 5 },
+    { name: "max", age: 40 },
+    { name: "john", age: 15 }
+  ];
 
-// test('returns correct index', () => {
-//   const objectsArr = [
-//     { x: 1, y: 3, z: 5 },
-//     { name: 'max', age: 40 },
-//     { name: 'john', age: 15 }
-//   ];
+  const foundIndex = findEqualObject(objectsArr, { name: "john", age: 15 });
 
-//   const foundIndex = findEqualObject(objectsArr, { name: 'john', age: 15 });
+  expect(foundIndex).toBe(2);
+});
 
-//   expect(foundIndex).toBe(2);
-// });
+test("returns -1 if there is no equal objects found", () => {
+  const objectsArr = [
+    { x: 1, y: 3, z: 5 },
+    { name: "max", age: 40 },
+    { name: "john", age: 15 }
+  ];
 
-// test('returns -1 if there is no equal objects found', () => {
-//   const objectsArr = [
-//     { x: 1, y: 3, z: 5 },
-//     { name: 'max', age: 40 },
-//     { name: 'john', age: 15 }
-//   ];
+  const foundIndex = findEqualObject(objectsArr, { name: "john", age: 17 });
 
-//   const foundIndex = findEqualObject(objectsArr, { name: 'john', age: 17 });
+  expect(foundIndex).toBe(-1);
+});
 
-//   expect(foundIndex).toBe(-1);
-// });
+describe("findEqualObject throws", () => {
+  test("if not all arguments are passed", () => {
+    const obj = { name: "john", age: 17 };
+    expect(() => findEqualObject(obj)).toThrow("Expected 2 arguments");
+  });
 
-// test('throws if not all arguments are passed', () => {
-//   expect(findEqualObject({ name: 'john', age: 17 }))
-//     .toThrow('Expected 2 arguments');
-// });
+  test("if the first argument is not an array", () => {
+    expect(() => findEqualObject({}, {}))
+      .toThrow("The first argument should be an array");
+  });
 
-// test('throws if the first argument is not an array', () => {
-//   const object = {
-//     1: { x: 1, y: 3, z: 5 },
-//     2: { name: 'max', age: 40 },
-//     3: { name: 'john', age: 15 }
-//   };
+  test("if the second argument is not an object", () => {
+    const objectsArr = [
+      { x: 1, y: 3, z: 5 },
+      { name: "max", age: 40 },
+      { name: "john", age: 15 }
+    ];
 
-//   expect(findEqualObject(object, { name: 'john', age: 17 }))
-//     .toThrow('The first argument should be an array');
-// });
-
-// test('throws if the second argument is not an object', () => {
-//   const objectsArr = [
-//     { x: 1, y: 3, z: 5 },
-//     { name: 'max', age: 40 },
-//     { name: 'john', age: 15 }
-//   ];
-
-//   expect(findEqualObject(objectsArr, '1'))
-//     .toThrow('The second argument should be an object');
-// });
+    expect(() => findEqualObject(objectsArr, "1")).toThrow(
+      "The second argument should be an object"
+    );
+  });
+});
